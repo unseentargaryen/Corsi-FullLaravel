472565325b372130cb0c47744d646ad0c763017e
tommaso.cisternino <tommycisternino@gmail.com>
2022-06-25 14:16:29 +0200

    Maintence mode added

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); //ha un middleware auth nella classe
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); //ha un middleware auth nella classe
+if (env('MAINTENCE') == true) {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); //ha un middleware auth nella classe
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); //ha un middleware auth nella classe
 
-Auth::routes();
+    Auth::routes();
 
-Route::prefix('admin')->group(function () {
-    Route::get('dashboard', [App\Http\Controllers\AdminDashboardController::class, 'index'])->middleware(['admin'])->name('admin-dashboard');
-    Route::get('login', [App\Http\Controllers\AdminDashboardController::class, 'login'])->name('admin-login');
-});
+    Route::prefix('admin')->group(function () {
+        Route::get('dashboard', [App\Http\Controllers\AdminDashboardController::class, 'index'])->middleware(['admin'])->name('admin-dashboard');
+        Route::get('login', [App\Http\Controllers\AdminDashboardController::class, 'login'])->name('admin-login');
+    });
+
+}
