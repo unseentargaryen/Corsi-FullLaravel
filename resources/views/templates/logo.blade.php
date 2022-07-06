fa848784201995b1c0f4ca03811367356f1e7fe2
tommaso.cisternino <tommycisternino@gmail.com>
2022-07-06 20:00:28 +0200

    cambio logo

--- resources/views/templates/logo.blade.php
+++ resources/views/templates/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="row mb-3">
     <div class="col-auto mx-auto">
-        <img src="{{asset("/images/logofrank.png")}}" class="img-fluid">
+        <img src="{{asset("/images/logo.png")}}" class="img-fluid">
     </div>
 </div>
