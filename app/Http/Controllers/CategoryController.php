522afa4e8f3ab0d60624dfeea0f88485a03b0108
tommaso.cisternino <tommycisternino@gmail.com>
2022-07-20 13:43:28 +0200

    category form edit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -38,10 +38,6 @@ class CategoryController extends Controller
 
     public function create(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required',
-        ]);
-
         $category = new Category();
         $category->name = $request->name;
 
