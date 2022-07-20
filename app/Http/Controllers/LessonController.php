be60bbd9d979a002c4def0a5d5884c0465c22879
tommaso.cisternino <tommycisternino@gmail.com>
2022-07-20 21:17:10 +0200

    fixing getLessonsByCourse

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -66,7 +66,7 @@ class LessonController extends Controller
         $start = $request->start;
         $end = $request->end;
 
-        $lessons = Lesson::where('course_id', $course_id)->whereDate('start', '>=', $start)->whereDate('start', '<=', $end)->whereDate('start', '>=', Carbon::now())->get();
+        $lessons = Lesson::where('course_id', $course_id)->whereDate('start', '>=', $start)->whereDate('start', '<=', $end)->whereDate('start', '>=', Carbon::tomorrow())->get();
         $_lessons = [];
         foreach ($lessons as $l) {
             $l->seats_available = ($l->max_participants - ($l->bookings()->count() + $l->pendingBookings()->count()));
