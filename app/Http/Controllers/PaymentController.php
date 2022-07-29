06813908909e0a5b6aae5dfdc0b3316e3b577e09
tommaso.cisternino <tommycisternino@gmail.com>
2022-07-29 21:00:48 +0200

    $this->gateway->setTestMode(false);

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -20,7 +20,7 @@ class PaymentController extends Controller
         $this->gateway = Omnipay::create('PayPal_Rest');
         $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
         $this->gateway->setSecret(env('PAYPAL_CLIENT_SECRET'));
-        $this->gateway->setTestMode(false);
+        $this->gateway->setTestMode(true);
     }
 
     public function pay(Request $request)
