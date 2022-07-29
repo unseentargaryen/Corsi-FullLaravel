7ac4f414edf1ab879888173301dba8109851ceb1
tommaso.cisternino <tommycisternino@gmail.com>
2022-07-29 20:04:48 +0200

    adjusted query

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -91,7 +91,7 @@ class LessonController extends Controller
         $start = $request->start;
         $end = $request->end;
 
-        $lessons = Lesson::where('course_id', $course_id)->whereDate('start', '>=', Carbon::create($start))->whereDate('start', '>=', Carbon::tomorrow())->get();
+        $lessons = Lesson::where('course_id', $course_id)->where('visible', true)->whereDate('start', '>=', Carbon::create($start))->whereDate('start', '>=', Carbon::tomorrow())->get();
         $_lessons = [];
         foreach ($lessons as $l) {
             $l->seats_available = ($l->max_participants - ($l->bookings()->count() + $l->pendingBookings()->count()));
